77210a0a656d214d01d2626bef55de67ec2e65b5
Chandru S <chandrusangar8@gmail.com>
2026-07-20 21:38:37 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,30 @@
+#add last postion use--append
+a=[1,2,3,4,5]
+a.append(7)
+print(a)
+print(a[1])
+
+#add exact position use--insert
+a=[1,2,3,4,5]
+a.insert(0,0)
+print(a)
+
+#to replace other use--below code
+a=[1,2,3,4,5]
+a[3]=11
+print(a)
+
+#remove particular index
+a=[1,3,4,5,6]
+a.pop(3)
+print(a)
+
+#last index dlt
+a=[1,3,4,5,6]
+a.pop()
+print(a)
+
+
+
+
+
