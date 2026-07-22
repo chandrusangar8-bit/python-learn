7e820e23bdad6932feeffa2885d35b7795b30ee9
Chandru S <chandrusangar8@gmail.com>
2026-07-22 22:32:34 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,33 @@
+def painter():
+  print("painting")
+painter()
+
+
+def add():
+  print("addition:")
+  a=int(input("a:"))
+  b=int(input("b:"))
+  print("add:",a+b)
+add()
+
+def sub():
+  print("subtraction:")
+  a=int(input("a:"))
+  b=int(input("b:"))
+  print("sub:",a-b)
+sub()
+
+
+def mul():
+  print("multiplication:")
+  a=int(input("a:"))
+  b=int(input("b:"))
+  print(mul)
+mul()
+
+def div():
+  print("division:")
+  a=int(input("a:"))
+  b=int(input("b:"))
+  print("div:",a/b)
+div()
